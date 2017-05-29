0f3980ab8a0beedbcf04c08d43086139b42b02bc
Ayaz Hussein <ayazhussein@gmail.com>
2017-05-29 10:20:16 +0300

    Updates

--- src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-export const USERS_URL = "https://timesheet-staging-aurity.herokuapp.com/api/users";
-
-export function generateMonthUrlForUser(month, year, userId) {
-    return `https://timesheet-staging-aurity.herokuapp.com/api/training/weeks/${month}/${year}/${userId}`;
-}
-export function generateApproveUrl(weekId, userId) {
-    return `https://timesheet-staging-aurity.herokuapp.com/api/training/weeks/${weekId}/users/${userId}`
-}
